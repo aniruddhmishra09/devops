20f798a3b816a0c9f867366a1fb1914917896a93
Aniruddh <apple@Apples-MacBook-Pro.local>
2025-08-11 19:35:36 +0530

    First Commit to add empty spring boot project

---
+++ jenkins/jenkins-with-springboot-app/src/main/java/com/study/devops/jenkins_with_springboot_app/JenkinsWithSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.study.devops.jenkins_with_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWithSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWithSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ jenkins/jenkins-with-springboot-app/src/test/java/com/study/devops/jenkins_with_springboot_app/JenkinsWithSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.devops.jenkins_with_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWithSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
